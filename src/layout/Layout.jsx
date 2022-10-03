a19f57acc394aba2e12aa7f7351829b73c493974
nnvedward <nnvedward@gmail.com>
2022-10-03 22:01:32 +0100

    Added header and nav component

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
+import Header from '../components/header/Header'
+import Nav from '../components/nav/Nav'
 import './layout.css'
 
 const layout = () => {
-  return (
-    <div className='layout'>
-        <div className='header'>Header</div>
-        <div className='nav'>Nav</div>
-        <div className='main'>Main</div>
-        <div className='footer'>footer</div>
-    </div>
-  )
+    return (
+        <div className='layout'>
+            <div className='header-container'>
+                <Header />
+            </div>
+            <div className='nav-container'>
+                <Nav />
+            </div>
+            <div className='main-container'>Main</div>
+            <div className='footer-container'>footer</div>
+        </div>
+    )
 }
 
 export default layout
