e7f32987efd4a3a6d7c09c0b041e7f9689057301
nnvedward <nnvedward@gmail.com>
2022-10-03 22:02:03 +0100

    Added header and nav component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className='logo'>Exam system</div>
+        <div className='headerItems'>
+            <div>Header1</div>
+            <div>Header2</div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './nav.css'
+
+const Nav = () => {
+  return (
+    <div className='nav'>
+        <div>Dashboard</div>
+        <div>Questions</div>
+        <div>Assessments</div>
+        <div>Candidates</div>
+        <div>Settings</div>
+    </div>
+  )
+}
+
+export default Nav
