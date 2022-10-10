84913390f5c1faf51c0a57931cc978f2e19f5913
nnvedward <nnvedward@gmail.com>
2022-10-10 11:44:59 +0100

    Set card links and icons

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './card.css'
 
-const Card = ({title, icon, details}) => {
-  return (
-    <div className="card">
-        <figure className="card-box">
-            <div className="card-header">
-                <h4>{title}</h4>
-                <p>{icon}</p>
-            </div>
-            <div className="card-details">
-                <p>{details}</p>
-            </div>
-        </figure>
-    </div>
-  )
+const Card = ({ path, title, icon, details }) => {
+    return (
+        <div className="card">
+            <Link to={path} className="card-link">
+                <figure className="card-box">
+                    <div className="card-header">
+                        <h4>{title}</h4>
+                        <p>{icon}</p>
+                    </div>
+                    <div className="card-details">
+                        <p>{details}</p>
+                    </div>
+                </figure>
+            </Link>
+        </div>
+    )
 }
 
 export default Card
--- src/pages/question/Question.jsx
+++ src/pages/question/Question.jsx
@@ -5,14 +5,33 @@ import "./question.css"
 const Question = () => {
   return (
     <div className="questions">
-      <Card title="Add Question" icon={<ion-icon name="newspaper-outline" class="nav-icon"></ion-icon>
-      } details="Create new question for a test" />
-      <Card title="Question Bank" icon={<ion-icon name="newspaper-outline" class="nav-icon"></ion-icon>
-      } details="View all the questions created" />
-      <Card title="Question Category" icon={<ion-icon name="newspaper-outline" class="nav-icon"></ion-icon>
-      } details="View all question categories" />
-      <Card title="Marks" icon={<ion-icon name="newspaper-outline" class="nav-icon"></ion-icon>
-      } details="View all marks in all assessments" />
+      <Card
+        path="add-question"
+        title="Add Question"
+        icon={<ion-icon name="pencil-outline" class="card-icon"></ion-icon>}
+        details="Create new question for a test"
+      />
+
+      <Card
+        path="question-bank"
+        title="Question Bank"
+        icon={<ion-icon name="albums-outline" class="card-icon"></ion-icon>}
+        details="View all the questions created"
+      />
+
+      <Card
+        path="question-category"
+        title="Question Category"
+        icon={<ion-icon name="copy-outline" class="card-icon"></ion-icon>}
+        details="View all question categories"
+      />
+
+      <Card
+        path="marks"
+        title="Marks"
+        icon={<ion-icon name="help-outline" class="card-icon"></ion-icon>}
+        details="View all marks in all assessments"
+      />
     </div>
   )
 }
