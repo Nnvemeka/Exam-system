82e6593042f5051517543e153184c055c3a5acab
nnvedward <nnvedward@gmail.com>
2022-10-10 12:31:25 +0100

    Added avatar and changed design color

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,8 +6,10 @@ const Header = () => {
     <div className='header'>
         <div className='logo'>Exam system</div>
         <div className='headerItems'>
-            <div>Header1</div>
-            <div>Header2</div>
+            <div className='avatar'>
+              <img src='./avatar.jpg' alt='avatar' className='header-img'/>
+            </div>
+            <div className='header-title'>Admin</div>
         </div>
     </div>
   )
