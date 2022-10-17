5cc6c6a4694bec353314fb0c4772eace18c15f9e
nnvedward <nnvedward@gmail.com>
2022-10-17 22:57:01 +0100

    Setup navbar active links

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,45 +1,84 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import './nav.css'
 
 const Nav = () => {
+  let activeStyle = {
+    display: "flex",
+    alignItems: "center",
+    textDecoration: "none",
+    padding: '1.4rem 0 2rem 3rem',
+    backgroundColor: '#ebfbee'
+  };
+
   return (
-    <div className='nav'>
+    <nav className='nav'>
       <div className='nav-item'>
-        <Link to={'/'} className="link">
+        <NavLink
+          to={'/'}
+          end
+          style={({ isActive }) =>
+            isActive ? activeStyle : undefined
+          }
+          className="link"
+        >
           <ion-icon class="nav-icon" name="home-outline"></ion-icon>
           <span className="nav-title">Dashboard</span>
-        </Link>
+        </NavLink>
       </div>
 
       <div className='nav-item'>
-        <Link to={'/questions'} className="link">
+        <NavLink
+          to={'/questions'}
+          style={({ isActive }) =>
+            isActive ? activeStyle : undefined
+          }
+          className="link"
+        >
           <ion-icon name="newspaper-outline" class="nav-icon"></ion-icon>
           <span className="nav-title">Questions</span>
-        </Link>
+        </NavLink>
       </div>
 
       <div className='nav-item'>
-        <Link to={'/assessments'} className="link">
+        <NavLink
+          to={'/assessments'}
+          style={({ isActive }) =>
+            isActive ? activeStyle : undefined
+          }
+          className="link"
+        >
           <ion-icon name="list-outline" class="nav-icon"></ion-icon>
           <span className="nav-title">Assessments </span>
-        </Link>
+        </NavLink>
       </div>
 
       <div className='nav-item'>
-        <Link to={'/candidates'} className="link">
+        <NavLink
+          to={'/candidates'}
+          style={({ isActive }) =>
+            isActive ? activeStyle : undefined
+          }
+          className="link"
+        >
           <ion-icon name="people-outline" class="nav-icon"></ion-icon>
           <span className="nav-title">Candidates</span>
-        </Link>
+        </NavLink>
       </div>
 
       <div className='nav-item'>
-        <Link to={'/settings'} className="link">
+        <NavLink
+          to={'/settings'}
+          style={({ isActive }) =>
+            isActive ? activeStyle : undefined
+          }
+          className="link"
+        >
           <ion-icon name="settings-outline" class="nav-icon"></ion-icon>
           <span className="nav-title">Settings</span>
-        </Link>
+        </NavLink>
       </div>
-    </div>
+    </nav>
   )
 }
 
