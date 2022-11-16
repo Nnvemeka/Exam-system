f693d2e5b347a55930379e67729abc0435347f9f
nnvedward <nnvedward@gmail.com>
2022-11-16 16:48:38 +0100

    Setup Edit modal for candidates

--- src/pages/candidates/Candidates.jsx
+++ src/pages/candidates/Candidates.jsx
@@ -1,12 +1,17 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import ReactModal from 'react-modal';
 import './candidate.css'
 
 const Candidates = () => {
+    const [onEditModal, setOnEditModal] = useState(false);
     const [onDeleteModal, setOnDeleteModal] = useState(false);
+
+    const openEditModal = () => setOnEditModal(true)
+    const closeEditModal = () => setOnEditModal(false)
 
     const openDeleteModal = () => setOnDeleteModal(true)
     const closeDeleteModal = () => setOnDeleteModal(false)
+
 
     return (
         <main className='candidate'>
@@ -32,11 +37,11 @@ const Candidates = () => {
                             <td>User</td>
                             <td>
                                 <div className='btn-container'>
-                                    {/* <button>
-                                        <Link to={`/question-category/edit/${cat.id}`}> */}
-                                            <ion-icon name="create-outline" class="action-icon edit"></ion-icon>
-                                        {/* </Link>
-                                    </button> */}
+                                    <button onClick={() => {
+                                        openEditModal()
+                                    }}>
+                                        <ion-icon name="create-outline" class="action-icon edit"></ion-icon>
+                                    </button>
                                     <button onClick={() => {
                                         openDeleteModal();
                                         // setId(cat.id);
@@ -50,6 +55,29 @@ const Candidates = () => {
                     </tbody>
                 </table>
             </section>
+
+            {/* Edit candidate modal */}
+            < ReactModal
+                isOpen={onEditModal}
+                onRequestClose={closeEditModal}
+                contentLabel="Edit Candidate"
+                ariaHideApp={false}
+                closeTimeoutMS={200}
+                className="modal"
+            >
+                <div className="modal-header">
+                    <h2>Edit Candidate</h2>
+                    <span onClick={closeEditModal}>x</span>
+                </div>
+                <div className="editInputs">
+                    <input type="text" className='editInput'/>
+                    <input type="text" className='editInput'/>
+                </div>
+                <div className="modal-button">
+                    <button className='modal-update-btn' >Confirm</button>
+                    <button className='modal-cancel-btn' onClick={closeEditModal}>Cancel</button>
+                </div>
+            </ReactModal>
 
             {/* Delete category modal */}
             < ReactModal
